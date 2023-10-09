2b814d304e822754497aa6153090014fcdf2145c
2Randoom <mathwedin@gmail.com>
2023-10-09 13:07:02 -0700

    3.3 is done
    the animations are done yay!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Assets/scrips/playercontoler.cs
+++ Assets/scrips/playercontoler.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playercontoler : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAm;
     public float jumpforce = 10f;
     public float gravitymodifier;
     public bool isonground = true;
@@ -15,16 +16,18 @@ public class playercontoler : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAm = GetComponent<Animator>();
         Physics.gravity *= gravitymodifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)&&isonground) 
+        if (Input.GetKeyDown(KeyCode.Space)&&isonground && !gameover) 
         {
             playerRb.AddForce(Vector3.up * jumpforce,ForceMode.Impulse);
             isonground = false;
+            playerAm.SetTrigger("Jump_trig");
         }
     }
 
@@ -40,6 +43,8 @@ public class playercontoler : MonoBehaviour
         {
             Debug.Log("Gameover");
             gameover = true;
+            playerAm.SetBool("Death_b", true);
+            playerAm.SetInteger("DeathType_int", 1);
         }
     }
 }
